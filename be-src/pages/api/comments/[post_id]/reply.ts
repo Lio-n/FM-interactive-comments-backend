d3df579f3c27755978a09e41ecbabcfddaa60e4b
Lion <leonardofontan14@gmail.com>
2022-06-07 14:13:04 -0300

    refactor (be-src/pages) : Remove folder '/pages'

--- be-src/pages/api/comments/[post_id]/reply.ts
+++
@@ -1,8 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import methods from "micro-method-router";
-
-const postComment = async (req: NextApiRequest, res: NextApiResponse) => {};
-
-export default methods({
-  post: postComment,
-});
--- be-src/pages/api/index.ts
+++
@@ -1,8 +0,0 @@
-/*  
-  # Los enpoints en general no utilizan directamente las Clases de los modelos.
-  # Estos enpoints solo se comunican con los controllers.
-*/
-import { NextApiResponse } from "next";
-export default (res: NextApiResponse) => {
-  res.status(418).send("Bienvenido");
-};
